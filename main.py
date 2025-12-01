d564be064c09cbe69267c6fe79e025372b251fb7
VanshSharma <vanshsharma2786@gmail.com>
2025-12-01 12:00:15 +0530

    Added menu before running the game with a Play button to start the game

--- main.py
+++ main.py
@@ -11,14 +11,42 @@ BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
 
 PLAYER_WIDTH = 30
 PLAYER_HEIGHT = 50
-
 PLAYER_VEL = 5
 
 STAR_WIDTH = 10
 STAR_HEIGHT = 20 
 STAR_VEL = 3
 
+WHITE = (255,255,255)
+RED = (255,0,0)
+BLACK = (0,0,0)
+
+
 FONT = pygame.font.SysFont("comicsans", 30)
+TITLE_FONT = pygame.font.SysFont("comicsans", 60)
+BUTTON_FONT = pygame.font.SysFont("comicsans", 40)
+
+
+def draw_button(text, x, y, width, height, color, hover_color):
+    mouse_pos = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()[0]
+
+    btn_rect = pygame.Rect(x, y, width, height)
+
+    if btn_rect.collidepoint(mouse_pos):
+        pygame.draw.rect(WIN, hover_color, btn_rect)
+        if click:
+            return True    
+    else:
+        pygame.draw.rect(WIN, color, btn_rect)
+
+
+    btn_text = BUTTON_FONT.render(text, 1, WHITE)
+    WIN.blit(btn_text, (x + (width - btn_text.get_width())//2,
+                        y + (height - btn_text.get_height())//2))
+
+    return False
+
 
 def draw(player, elapsed_time, stars):
     WIN.blit(BG, (0, 0))
@@ -32,6 +60,46 @@ def draw(player, elapsed_time, stars):
         pygame.draw.rect(WIN, "white", star)
 
     pygame.display.update() # This line here - make sure jo img lgai hai - refresh and apply changes
+
+
+def main_menu():
+    run = True
+
+    play_button = pygame.Rect(WIDTH//2 - 100, HEIGHT//2 - 25, 200, 50)
+
+    while run:
+
+        WIN.blit(BG, (0, 0))
+
+        title_text = TITLE_FONT.render("RAIN DODGE", 1, "white")
+        WIN.blit(title_text, (WIDTH//2 - title_text.get_width()//2, 150))
+
+       
+        pygame.draw.rect(WIN, "red", play_button)
+
+        
+        play_text = BUTTON_FONT.render("PLAY", 1, "white")
+        WIN.blit(
+            play_text,
+            (
+                play_button.x + (play_button.width - play_text.get_width()) // 2,
+                play_button.y + (play_button.height - play_text.get_height()) // 2,
+            ),
+        )
+
+   
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                return  #
+
+            if event.type == pygame.MOUSEBUTTONDOWN:   
+                mouse_pos = event.pos                  
+                if play_button.collidepoint(mouse_pos):
+                    return  
+
+        pygame.display.update()
 
 # For now - on running this program pygame window pops up and then vo disappear hojati hai immediately, that's because maine abhi koi loop nhi lga rkhi so that mera progeam alive rhe.
 
@@ -95,5 +163,7 @@ def main():
 
     pygame.quit()  
 
+
 if __name__ == "__main__":
+    main_menu()
     main()
